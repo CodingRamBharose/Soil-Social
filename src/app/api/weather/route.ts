a1ebf173d3057636c4c3d5c6c6f0f8c1c9510015
ramavtar07 <ramavtarsharma85507@gmail.com>
2025-07-21 21:13:12 +0530

    add weather api url

--- src/app/api/weather/route.ts
+++ src/app/api/weather/route.ts
@@ -12,7 +12,7 @@ export async function GET(request: Request) {
   }
 
   try {
-    // const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.OPENWEATHER_API_KEY}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.OPENWEATHER_API_KEY}&units=metric`;
     const response = await fetch(url);
     const data = await response.json();
 
